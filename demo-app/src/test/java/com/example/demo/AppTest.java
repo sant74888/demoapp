22f6c819d0e0a933073292d70c2e7fe38c25a24a
santosh <override007@gmail.com>
2025-03-30 06:56:13 -0400

    added

---
+++ demo-app/src/test/java/com/example/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
